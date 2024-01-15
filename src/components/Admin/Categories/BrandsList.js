1c0daf47af5e400e6b85b939995f94b04b4cd29a
Bilgvvn123 <dukd875@gmail.com>
2024-01-15 12:52:31 +0800

    a

--- src/components/Admin/Categories/BrandsList.js
+++ src/components/Admin/Categories/BrandsList.js
@@ -5,93 +5,99 @@ import { fetchBrandsAction } from "../../../redux/slices/categories/brandsSlice"
 import ErrorMsg from "../../ErrorMsg/ErrorMsg";
 import LoadingComponent from "../../LoadingComp/LoadingComponent";
 import NoDataFound from "../../NoDataFound/NoDataFound";
-const people = [
-  {
-    name: "Lindsay Walton",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-  // More people...
-];
+// const people = [
+//   {
+//     name: "Lindsay Walton",
+//     title: "Front-end Developer",
+//     email: "lindsay.walton@example.com",
+//     role: "Member",
+//   },
+//   // More people...
+// ];
 export default function BrandsList() {
-  //dispatch
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchBrandsAction());
-  }, [dispatch]);
-  const {
-    brands: { brands },
-    loading,
-    error,
-  } = useSelector((state) => state?.brands);
+	//dispatch
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchBrandsAction());
+	}, [dispatch]);
+	const {
+		brands: { brands },
+		loading,
+		error,
+	} = useSelector((state) => state?.brands);
 
-  //delete category handler
-  const deleteCategoryHandler = (id) => {};
-  return (
-    <div className="px-4 sm:px-6 lg:px-8">
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">
-            All Brands Categories [{brands?.length}]
-          </h1>
-          <p className="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title,
-          </p>
-        </div>
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-          <Link
-            to="/admin/add-category"
-            type="button"
-            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
-            Add New Category
-          </Link>
-        </div>
-      </div>
-      {loading ? (
-        <LoadingComponent />
-      ) : error ? (
-        <ErrorMsg message={error?.message} />
-      ) : brands?.length <= 0 ? (
-        <NoDataFound />
-      ) : (
-        <div className="mt-8 flex flex-col">
-          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                        Name
-                      </th>
+	//delete category handler
+	//   const deleteCategoryHandler = (id) => {};
+	return (
+		<div className="px-4 sm:px-6 lg:px-8">
+			<div className="sm:flex sm:items-center">
+				<div className="sm:flex-auto">
+					<h1 className="text-xl font-semibold text-gray-900">
+						All Brands Categories [{brands?.length}]
+					</h1>
+					<p className="mt-2 text-sm text-gray-700">
+						A list of all the users in your account including their
+						name, title,
+					</p>
+				</div>
+				<div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+					<Link
+						to="/admin/add-category"
+						type="button"
+						className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+					>
+						Add New Category
+					</Link>
+				</div>
+			</div>
+			{loading ? (
+				<LoadingComponent />
+			) : error ? (
+				<ErrorMsg message={error?.message} />
+			) : brands?.length <= 0 ? (
+				<NoDataFound />
+			) : (
+				<div className="mt-8 flex flex-col">
+					<div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+						<div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+							<div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+								<table className="min-w-full divide-y divide-gray-300">
+									<thead className="bg-gray-50">
+										<tr>
+											<th
+												scope="col"
+												className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+											>
+												Name
+											</th>
 
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                        Created At
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 bg-white">
-                    {brands?.map((brand) => (
-                      <tr key={brand?._id}>
-                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-                          <div className="flex items-center">
-                            <div className="ml-4">
-                              <div className="font-medium text-gray-900">
-                                {brand?.name}
-                              </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {new Date(brand?.createdAt).toLocaleDateString()}
-                        </td>
-                        {/* edit icon */}
-                        {/* <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-left text-sm font-medium sm:pr-6">
+											<th
+												scope="col"
+												className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+											>
+												Created At
+											</th>
+										</tr>
+									</thead>
+									<tbody className="divide-y divide-gray-200 bg-white">
+										{brands?.map((brand) => (
+											<tr key={brand?._id}>
+												<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+													<div className="flex items-center">
+														<div className="ml-4">
+															<div className="font-medium text-gray-900">
+																{brand?.name}
+															</div>
+														</div>
+													</div>
+												</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+													{new Date(
+														brand?.createdAt
+													).toLocaleDateString()}
+												</td>
+												{/* edit icon */}
+												{/* <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-left text-sm font-medium sm:pr-6">
                           <Link
                             to={`/admin/edit-category/${category?._id}`}
                             state={{
@@ -115,8 +121,8 @@ export default function BrandsList() {
                             <span className="sr-only">, {category?.name}</span>
                           </Link>
                         </td> */}
-                        {/* delete icon */}
-                        {/* <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-left text-sm font-medium sm:pr-6">
+												{/* delete icon */}
+												{/* <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-left text-sm font-medium sm:pr-6">
                           <button
                             onClick={() => deleteCategoryHandler(category?._id)}
                             className="text-indigo-600 hover:text-indigo-900">
@@ -135,15 +141,15 @@ export default function BrandsList() {
                             </svg>
                           </button>
                         </td> */}
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+											</tr>
+										))}
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
